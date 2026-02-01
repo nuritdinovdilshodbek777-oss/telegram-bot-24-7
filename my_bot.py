c9c7b3eb3a8e1ba24be5ea6c26a4188ddb31294f
nuritdinovdilshodbek777-oss <nuritdinovdilshodbek777@gmail.com>
2026-02-02 04:19:24 +0500

    Add files via upload

---
+++ my_bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+TOKEN = "7964768911:AAEhuV6B-vmwJpdJhnbguSqTyBX5_5tjXLQ"
+
+async def auto_delete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message:
+        await update.message.delete()
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+# Faqat URL larni o'chiradi
+app.add_handler(MessageHandler(filters.URL, auto_delete))
+
+print("Bot ishlayapti...")
+app.run_polling()
